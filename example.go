b55fc9f1b0aa1739c58186ddaca11e0ec9ddcfc4
宁金 <645619776@qq.com>
2018-08-27 00:24:00 +0800

    gin practice

--- example.go
+++ example.go
@@ -1,6 +1,9 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func main() {
 	r := gin.Default()
@@ -10,5 +13,29 @@ func main() {
 		})
 	})
 
+	r.GET("test", test)
+	r.GET("/user/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		c.String(http.StatusOK, "Hello %s", name)
+	})
+
+	r.GET("/user/:name/*action", func(c *gin.Context) {
+		name := c.Param("name")
+		action := c.Param("action")
+		message := name + " is " + action
+		c.String(http.StatusOK, message)
+	})
+
+	r.GET("/welcome", func(c *gin.Context) {
+		firstname := c.DefaultQuery("firstname", "Guest")
+		lastname := c.Query("lastname")
+		c.String(http.StatusOK, "Hello %s %s", firstname, lastname)
+	})
 	r.Run()
+}
+
+func test(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "test",
+	})
 }
